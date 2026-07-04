31ebd6836b43401538b09121bac8df5e888bf898
divya <282848370+divyavansh060-blip@users.noreply.github.com>
2026-07-04 20:33:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,20 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/script.js"]);
-    })
+const CACHE_NAME = 'moonlight-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Great+Vibes&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
